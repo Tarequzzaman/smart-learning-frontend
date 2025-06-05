00d087f3ef5b6217bdf9658ba6ba2c1c3cfa39b0
Tarequzzaman <tareqcse12@gmail.com>
2025-06-05 23:54:39 +1000

    vite config upadted

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // optional: raise warning limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+        },
+      },
+    },
+  },
 })
-
-
